f56de83b2eb81d83e09e4ca39ae27898617e613a
Juvid <jutewei@qq.com>
2018-04-04 09:22:15 +0800

    Initial Commit

---
+++ JuImageZoomScale/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JuImageZoomScale
+//
+//  Created by Juvid on 2018/4/4.
+//  Copyright © 2018年 Juvid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JuImageZoomScale/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JuImageZoomScale
+//
+//  Created by Juvid on 2018/4/4.
+//  Copyright © 2018年 Juvid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
